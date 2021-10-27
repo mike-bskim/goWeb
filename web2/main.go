36d5a556b34f99dfb1e08308b6d16d5d44b5d415
mike.bskim <mike.bskim@gmail.com>
2021-10-28 00:40:54 +0200

    #2 - json

--- web2/main.go
+++ web2/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"time"
@@ -9,23 +10,37 @@ import (
 const portNumber = ":3000"
 
 type User struct {
-	FirstName string
-	LastName  string
-	Email     string
-	CreatedAt time.Time
+	FirstName string    `json:"first_name"`
+	LastName  string    `json:"last_name"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
 }
 
+/*
+{
+	"FirstName":"bs",
+	"LastName":"kim",
+	"Email":"kimbs@kimbs.com"
+}
+*/
 type fooHandler struct{}
 
 func (f *fooHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	// user := new(User)
-	// err := json.NewDecoder(r.Body).Decode(user)
-	// if err != nil {
-	// 	w.WriteHeader(http.StatusBadRequest)
-	// 	fmt.Fprint(w, "Bad Request: ", err)
-	// 	return
-	// }
-	fmt.Fprintf(w, "Hello Foo!")
+	user := new(User)
+	// request 에서 값을 가져와서 디코딩후 user 형태로 변환.
+	err := json.NewDecoder(r.Body).Decode(user)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, "Bad Request: ", err)
+		return
+	}
+	user.CreatedAt = time.Now()
+	data, _ := json.Marshal(user)
+	// content type 을 json 으로 보내면 클라이언트에서 보기 좋게 표시됨
+	w.Header().Add("content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, string(data))
+	// fmt.Fprintf(w, "Hello Foo!")
 }
 
 func barHandler(w http.ResponseWriter, r *http.Request) {
