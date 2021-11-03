0e0fd45e61bc5ca1d763b273f704d5422c16094e
mike.bskim <mike.bskim@gmail.com>
2021-11-04 00:18:43 +0100

    REST api

--- web05/myapp/app.go
+++ web05/myapp/app.go
@@ -2,11 +2,25 @@
 package myapp
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/gorilla/mux"
 )
+
+type User struct {
+	Id        int       `json:"id"`
+	FirstName string    `json:"first_name"`
+	LastName  string    `json:"last_name"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+var userMap map[int]*User
+var lastId int
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello World")
@@ -17,18 +31,66 @@ func usersHandler(w http.ResponseWriter, r *http.Request) {
 	// Get UserInfo by /users/{id}
 }
 
+func createUserHandler(w http.ResponseWriter, r *http.Request) {
+	user := new(User)
+	err := json.NewDecoder(r.Body).Decode(user)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, err)
+		return
+	}
+
+	// created User
+	lastId += 1
+	user.Id = lastId
+	user.CreatedAt = time.Now()
+	userMap[user.Id] = user
+
+	w.Header().Add("content-type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	data, _ := json.Marshal(user)
+	fmt.Fprint(w, string(data))
+}
+
 func getUserInfoHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	fmt.Fprint(w, "User ID:", vars["id"])
+	// fmt.Fprint(w, "User ID:", vars["id"])
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, err)
+		return
+	}
+
+	// user := new(User)
+	// user.Id = id
+	// user.FirstName = "bs"
+	// user.LastName = "kim"
+	// user.Email = "kimbs@kimbs.com"
+
+	user, ok := userMap[id]
+	if !ok {
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprint(w, "No User ID:", id)
+		return
+	}
+
+	w.Header().Add("content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	data, _ := json.Marshal(user)
+	fmt.Fprint(w, string(data))
 }
 
 // making a new my handler
 func NewHttpHandler() http.Handler {
+	userMap = make(map[int]*User)
+	lastId = 0
 	// 인스턴스를 만들고 해당 인스턴스에 등록해서 사용하는 예제 코드.
 	mux := mux.NewRouter() // gorilla mux 사용법
 	// mux := http.NewServeMux() //gorilla mux 로 대체
 	mux.HandleFunc("/", indexHandler)
-	mux.HandleFunc("/users", usersHandler)
+	mux.HandleFunc("/users", usersHandler).Methods("GET")
+	mux.HandleFunc("/users", createUserHandler).Methods("POST")
 	mux.HandleFunc("/users/{id:[0-9]+}", getUserInfoHandler)
 	// mux.Handle("/foo", &fooHandler{})
 
--- web05/myapp/app_test.go
+++ web05/myapp/app_test.go
@@ -1,10 +1,13 @@
 package myapp
 
 import (
+	"encoding/json"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/http/httptest"
+	"strconv"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -47,9 +50,37 @@ func TestGetUserInfo(t *testing.T) {
 	assert.Equal(http.StatusOK, resp.StatusCode)
 	data, _ := ioutil.ReadAll(resp.Body)
 	log.Println(string(data))
-	assert.Contains(string(data), "User ID:90")
+	assert.Contains(string(data), "No User ID:90")
 }
 
 /*
 go get -u github.com/gorilla/mux
 */
+
+func TestCreateUser(t *testing.T) {
+	assert := assert.New(t)
+
+	ts := httptest.NewServer((NewHttpHandler()))
+	defer ts.Close()
+
+	resp, err := http.Post(ts.URL+"/users", "application/json",
+		strings.NewReader(`{"first_name":"bs", "last_name":"kim", "email":"kimbs@kimbs.com"}`))
+	assert.NoError(err)
+	assert.Equal(http.StatusCreated, resp.StatusCode)
+
+	user := new(User)
+	err = json.NewDecoder(resp.Body).Decode(user)
+	assert.NoError(err)
+	assert.NotEqual(0, user.Id)
+
+	id := user.Id
+	resp, err = http.Get(ts.URL + "/users/" + strconv.Itoa(id))
+	assert.NoError(err)
+	assert.Equal(http.StatusOK, resp.StatusCode)
+	user2 := new(User)
+	err = json.NewDecoder(resp.Body).Decode(user2)
+	assert.NoError(err)
+	assert.Equal(user.Id, user2.Id)
+	assert.Equal(user.FirstName, user2.FirstName)
+
+}
