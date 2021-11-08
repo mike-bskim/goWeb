45ae6686b85713a8acc375b873288fd78a6b333a
mike.bskim <mike.bskim@gmail.com>
2021-11-08 16:16:48 +0100

    docorator handler with logger

---
+++ web10/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"GO/tuckersGo/goWeb/web10/decoHandler"
+	"GO/tuckersGo/goWeb/web10/myapp"
+	"log"
+	"net/http"
+	"time"
+)
+
+// "net/http"
+
+// "github.com/tuckersGo/goWeb/web10/decoHandler"
+// "github.com/tuckersGo/goWeb/web10/myapp"
+
+const portNumber = ":3000"
+
+// func logger(w http.ResponseWriter, r *http.Request, h http.Handler) {
+// 	start := time.Now()
+// 	log.Println("[LOGGER1] Started")
+// 	h.ServeHTTP(w, r)
+// 	log.Println("[LOGGER1] Completed time:", time.Since(start).Milliseconds())
+// }
+
+// func logger2(w http.ResponseWriter, r *http.Request, h http.Handler) {
+// 	start := time.Now()
+// 	log.Println("[LOGGER2] Started")
+// 	h.ServeHTTP(w, r)
+// 	log.Println("[LOGGER2] Completed time:", time.Since(start).Milliseconds())
+// }
+
+// func NewHandler() http.Handler {
+// 	h := myapp.NewHandler()
+// 	h = decoHandler.NewDecoHandler(h, logger)
+// 	h = decoHandler.NewDecoHandler(h, logger2)
+// 	return h
+// }
+
+func logger1(w http.ResponseWriter, r *http.Request, h http.Handler) {
+	start := time.Now()
+	log.Println("[LOGGER1] Started")
+	h.ServeHTTP(w, r)
+	log.Printf("[LOGGER1] Completed, time: %v ms\n", time.Since(start).Milliseconds())
+}
+
+func NewHandler() http.Handler {
+	mux := myapp.NewHttpHandler()
+	mux = decoHandler.NewDecoHandler(mux, logger1)
+	return mux
+}
+
+func main() {
+	mux := NewHandler()
+
+	http.ListenAndServe(portNumber, mux)
+}
