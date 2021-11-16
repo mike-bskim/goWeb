860073fbd495c8200d948e408d48f076446f04b1
mike.bskim <mike.bskim@gmail.com>
2021-11-16 15:48:59 +0100

    todo

--- web16-todo/myapp/app.go
+++ web16-todo/myapp/app.go
@@ -2,6 +2,7 @@ package myapp
 
 import (
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/gorilla/mux"
@@ -31,6 +32,48 @@ func getTodoListHandler(w http.ResponseWriter, r *http.Request) {
 	rd.JSON(w, http.StatusOK, list)
 }
 
+func addTodoHandler(w http.ResponseWriter, r *http.Request) {
+	name := r.FormValue("name")
+	id := len(todoMap) + 1
+	todo := &Todo{id, name, false, time.Now()}
+	todoMap[id] = todo
+	rd.JSON(w, http.StatusOK, todo)
+}
+
+type Success struct {
+	Success bool `json:"success"`
+}
+
+func removeTodoHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, _ := strconv.Atoi(vars["id"])
+	if _, ok := todoMap[id]; ok {
+		delete(todoMap, id)
+		// log.Println("delete success")
+		// for i := 1; i < len(todoMap)+1; i++ {
+		// 	log.Println(todoMap[i])
+		// }
+		rd.JSON(w, http.StatusOK, Success{Success: true})
+	} else {
+		// log.Println("delete false", todoMap)
+		rd.JSON(w, http.StatusOK, Success{Success: false})
+	}
+
+}
+
+func completeTodoHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, _ := strconv.Atoi(vars["id"])
+	complete := r.FormValue("complete") == "true"
+	if todo, ok := todoMap[id]; ok {
+		todo.Completed = complete
+		rd.JSON(w, http.StatusOK, Success{Success: true})
+	} else {
+		rd.JSON(w, http.StatusOK, Success{Success: false})
+	}
+
+}
+
 func addTestTodos() {
 	todoMap[1] = &Todo{1, "Buy a milk", false, time.Now()}
 	todoMap[2] = &Todo{2, "Exercise", true, time.Now()}
@@ -44,5 +87,8 @@ func MakeNewHandler() http.Handler {
 	mux := mux.NewRouter()
 	mux.HandleFunc("/", indexHandler)
 	mux.HandleFunc("/todos", getTodoListHandler).Methods("GET")
+	mux.HandleFunc("/todos", addTodoHandler).Methods("POST")
+	mux.HandleFunc("/todos/{id:[0-9]+}", removeTodoHandler).Methods("DELETE")
+	mux.HandleFunc("/complete-todo/{id:[0-9]+}", completeTodoHandler).Methods("GET")
 	return mux
 }
