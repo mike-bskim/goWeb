b833fed8a39d0c97e9484998e75ec5eb0fc86402
mike.bskim <mike.bskim@gmail.com>
2021-11-16 18:09:30 +0100

    todo refactoring

---
+++ web18-todo_refactoring/myapp/app.go
@@ -0,0 +1,68 @@
+package myapp
+
+import (
+	"GO/tuckersGo/goWeb/web16-todo_refactoring/model"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	"github.com/unrolled/render"
+)
+
+var rd *render.Render
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "/todo.html", http.StatusTemporaryRedirect)
+}
+
+func getTodoListHandler(w http.ResponseWriter, r *http.Request) {
+
+	list := model.GetTodos()
+	rd.JSON(w, http.StatusOK, list)
+}
+
+func addTodoHandler(w http.ResponseWriter, r *http.Request) {
+	name := r.FormValue("name")
+	todo := model.AddTodo(name)
+	rd.JSON(w, http.StatusCreated, todo)
+}
+
+type Success struct {
+	Success bool `json:"success"`
+}
+
+func removeTodoHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, _ := strconv.Atoi(vars["id"])
+	ok := model.RemoveTodo(id)
+	if ok {
+		rd.JSON(w, http.StatusOK, Success{Success: true})
+	} else {
+		rd.JSON(w, http.StatusOK, Success{Success: false})
+	}
+}
+
+func completeTodoHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, _ := strconv.Atoi(vars["id"])
+	complete := r.FormValue("complete") == "true"
+	ok := model.CompleteTodo(id, complete)
+	if ok {
+		rd.JSON(w, http.StatusOK, Success{Success: true})
+	} else {
+		rd.JSON(w, http.StatusOK, Success{Success: false})
+	}
+}
+
+func MakeNewHandler() http.Handler {
+	// model.TodoMap = make(map[int]*model.Todo)
+	// addTestTodos()
+	rd = render.New()
+	mux := mux.NewRouter()
+	mux.HandleFunc("/", indexHandler)
+	mux.HandleFunc("/todos", getTodoListHandler).Methods("GET")
+	mux.HandleFunc("/todos", addTodoHandler).Methods("POST")
+	mux.HandleFunc("/todos/{id:[0-9]+}", removeTodoHandler).Methods("DELETE")
+	mux.HandleFunc("/complete-todo/{id:[0-9]+}", completeTodoHandler).Methods("GET")
+	return mux
+}
